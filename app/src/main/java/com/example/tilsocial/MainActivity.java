09b3f287488f5c777ed63355b8baf67ed4452ae3
Akshita.Sharma <akshusharma1526@gmail.com>
2021-07-29 21:36:14 +0530

    Updated

--- app/src/main/java/com/example/tilsocial/MainActivity.java
+++ app/src/main/java/com/example/tilsocial/MainActivity.java
@@ -19,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
         SigninFragment signinfragment = new SigninFragment();
 
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();

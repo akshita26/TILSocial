4e0b4c046e6e3e2b03773283ebbce0d964bc4f48
Manpreet.Kamboj <manpreetkamboj876@gmail.com>
2021-07-30 09:08:24 +0530

    adding comment

---
+++ app/src/main/java/com/example/tilsocial/CommentFragment.java
@@ -0,0 +1,28 @@
+package com.example.tilsocial;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CommentFragment extends Fragment {
+
+
+    public CommentFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+       View view = inflater.inflate(R.layout.fragment_comment, container, false);
+
+       return view;
+
+    }
+}

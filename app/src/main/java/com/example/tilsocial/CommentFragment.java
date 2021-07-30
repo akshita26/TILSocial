8f52d0e9b8573bc30997f55d2ef2d51cf7883a05
Manpreet.Kamboj <manpreetkamboj876@gmail.com>
2021-07-30 09:12:29 +0530

    comment

--- app/src/main/java/com/example/tilsocial/CommentFragment.java
+++ app/src/main/java/com/example/tilsocial/CommentFragment.java
@@ -12,14 +12,14 @@ public class CommentFragment extends Fragment {
 
 
     public CommentFragment() {
-        // Required empty public constructor
+
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
        View view = inflater.inflate(R.layout.fragment_comment, container, false);
 
        return view;
